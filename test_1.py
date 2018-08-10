e4ae805d1318fe33267319bb968aa088ef1ca564
Polina-Goroshko <40487792+Polina-Goroshko@users.noreply.github.com>
2018-08-10 12:47:22 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,10 +1,11 @@
 from logger_creation import log
+import os
 
 
 def test_1(main_setup_fixture, perm_for_export_creation, session_with_client_creation, parser_of_command_line):
 	log.info("I am in a test_1: file creation")
 	log.info("Here is what now is in /etc/exports %s", perm_for_export_creation[0])
-	stdin, stdout, stderr = session_with_client_creation.exec_command("echo %s | sudo -S touch /home/$USER/dirForMount/cinema" % (parser_of_command_line[3]))
+	stdin, stdout, stderr = session_with_client_creation.exec_command("echo %s | sudo -S touch /home/%s/dirForMount/cinema" % (parser_of_command_line[3], % os.getlogin()))
 	log.info("%s", stderr.read())
 	log.info("permForExportCreation[1] in test_1: %s", perm_for_export_creation[1])
 	if "(ro)" == perm_for_export_creation[1]:
